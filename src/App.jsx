51dec7abc309ecfc699ff52ff428f30afda54ee6
vnshas <vnshas@gmail.com>
2024-12-26 14:40:19 -0300

    finalizado

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { DefaultTemplate } from "./components/DefaultTemplate"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <DefaultTemplate>
+        <h1>Oi, eu sou 1 elemento de teste</h1>
+      </DefaultTemplate>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/DefaultTemplate/index.jsx
@@ -0,0 +1,16 @@
+import { Footer } from "../Footer"
+import { Header } from "../Header"
+
+export const DefaultTemplate = ({children}) =>{
+    return (
+        <>
+        <Header/>
+
+        <main>
+            {children}
+        </main>
+
+        <Footer/>
+        </>
+    )
+}
